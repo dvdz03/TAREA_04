63f8647b4f2faaa2c3a7a7870f60debe7bfb2fdd
dvdz03 <dflores04@alumnos.uaq.mx>
2025-04-12 01:09:35 -0600

    ejercicio ratón

--- SCRIPTS/RATON.R
+++ SCRIPTS/RATON.R
@@ -5,4 +5,40 @@ vcount(redbio)
 ecount(redbio)
 plot(redbio)
 help(layout)
-plot(redbio,layout=layout.auto)
+#todos los layouts posibles
+plot(redbio,layout=layout.auto,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.circle,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.davidson.harel,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.drl,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.fruchterman.reingold,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.fruchterman.reingold.grid,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.gem,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.graphopt,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.grid,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout_on_grid,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.kamada.kawai,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.lgl,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.mds,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.random,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout_as_tree,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.sphere,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.spring,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.star,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.sugiyama,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.svd,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout_components,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout_in_circle,vertex.size=15,edge.arrow.size=0.1)
+plot(redbio,layout=layout.grid,vertex.size=15,edge.arrow.size=0.1)
+
+#densidad de la red
+edge_density(redbio)
+
+#clusterización 
+transitivity(redbio, type=("local"))->rebio1
+transitivity(redbio, type="barrat")->rebio2
+transitivity(redbio, type="weighted")->rebio3
+
+#gráficas
+plot(rebio1)
+plot(rebio2)
+plot(rebio3)
